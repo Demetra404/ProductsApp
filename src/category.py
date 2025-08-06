90bf441a0956d72640bbd96f9bdefd3d46eaaf66
demetra <jennyani98@gmail.com>
2025-08-06 15:23:59 +0300

    fixed app on mypy

--- src/category.py
+++ src/category.py
@@ -1,3 +1,6 @@
+from typing import Any, Union
+
+
 class Category:
     name: str
     description: str
@@ -5,7 +8,7 @@ class Category:
     category_count: int = 0
     product_count: int = 0
 
-    def __init__(self, name: str, description: str, products=None):
+    def __init__(self, name: str, description: str, products: Union[list[Any], None] = None):
         self.name = name
         self.description = description
         self.products = products if products else []
--- src/to_json.py
+++ src/to_json.py
@@ -1,19 +1,23 @@
 import json
 import os
-from src.products import Product
+from typing import Any, List
+
 from src.category import Category
+from src.products import Product
 
 path_to_file = os.path.join(os.path.dirname(os.path.dirname(__file__)),
                             'data', 'products.json')
 
 
-def open_file_products(path):
-    with open(path_to_file, 'r', encoding='utf-8') as file:
+def open_file_products(path: str) -> List[Any]:
+    with open(path, 'r', encoding='utf-8') as file:
         data_products = json.load(file)
-    return data_products
+    return data_products if isinstance(data_products, list) else []
 
 
-def create_object_from_data(data):
+def create_object_from_data(data: List[Any]) -> List[Any]:
+    if data is []:
+        return []
     categories = []
     for object_data in data:
         products = []
@@ -28,6 +32,7 @@ def create_object_from_data(data):
 
 if __name__ == '__main__':
     raw_data = open_file_products(path_to_file)
+    print(type(raw_data))
     categories_data = create_object_from_data(raw_data)
     print(categories_data)
     print(categories_data[0].name)
