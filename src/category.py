1d4d6cb4e5f948c4173435822e46a0042b61c2ee
demetra <jennyani98@gmail.com>
2025-08-06 14:54:10 +0300

    fixed on flake8

--- src/category.py
+++ src/category.py
@@ -4,7 +4,8 @@ class Category:
     products: list
     category_count: int = 0
     product_count: int = 0
-    def __init__(self, name, description, products=None):
+
+    def __init__(self, name: str, description: str, products=None):
         self.name = name
         self.description = description
         self.products = products if products else []
--- src/to_json.py
+++ src/to_json.py
@@ -3,20 +3,28 @@ import os
 from src.products import Product
 from src.category import Category
 
-path_to_file = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data', 'products.json')
+path_to_file = os.path.join(os.path.dirname(os.path.dirname(__file__)),
+                            'data', 'products.json')
+
+
 def open_file_products(path):
     with open(path_to_file, 'r', encoding='utf-8') as file:
         data_products = json.load(file)
     return data_products
 
+
 def create_object_from_data(data):
-    categories =[]
+    categories = []
     for object_data in data:
         products = []
         for product in object_data['products']:
-            products.append(Product(product['name'], product['description'], product['price'],product['quantity']))
-        categories.append(Category(object_data['name'],object_data['description'], products))
+            products.append(Product(product['name'], product['description'],
+                                    product['price'], product['quantity']))
+        categories.append(Category(object_data['name'],
+                                   object_data['description'],
+                                   products))
     return categories
+
 
 if __name__ == '__main__':
     raw_data = open_file_products(path_to_file)
