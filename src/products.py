7acb5a29507afbcecbb9f2e11fe8f08050f807a7
demetra <jennyani98@gmail.com>
2025-08-13 13:46:17 +0300

    fixed function new_product in class Product

--- src/products.py
+++ src/products.py
@@ -5,36 +5,29 @@ class Product:
     name: str
     description: str
     quantity: int
-    products: list = []
 
     def __init__(self, name: str, description: str, price: float, quantity: int):
         self.name = name
         self.description = description
         self.__price = price
         self.quantity = quantity
-        """Доп к заданию 3 (кривовато)))"""
-        if Product.products is []:
-            Product.products.append([self.name, self.description, self.__price, self.quantity])
-        else:
-            for product in Product.products:
-                if product[0] == self.name:
-                    product[3] += quantity
-                    self.quantity = product[3]
-                    if price > product[2]:
-                        product[2] = price
-                        self.__price = product[2]
-                    else:
-                        self.__price = product[2]
-                    return
-            Product.products.append([self.name, self.description, self.__price, self.quantity])
 
     @classmethod
-    def new_product(cls, dict_products: dict) -> Self:
+    def new_product(cls, dict_products: dict, product_list: list = None) -> Self:
         name = dict_products['name']
         description = dict_products['description']
         price = dict_products['price']
         quantity = dict_products['quantity']
-        return cls(name, description, price, quantity)
+        new_product = cls(name, description, price, quantity)
+        if product_list:
+            for product in product_list:
+                if product.name == name:
+                    product.quantity += quantity
+                    if price > product.price:
+                        product.price = price
+                    return product
+            product_list.append(new_product)
+        return new_product
 
     @property
     def price(self) -> float:
